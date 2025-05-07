4d2f03c8b549ea129a68613e79dab3d3f6bfa15f
pihd <peggyhery@gmail.com>
2025-05-07 13:36:31 +0800

    v1

---
+++ ChatGPT_hw/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ChatGPT_hw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblRece = new System.Windows.Forms.Label();
+            this.lblSend = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtRece = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.lblContent = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblChange = new System.Windows.Forms.Label();
+            this.txtChange = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(36, 25);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(52, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "主題";
+            // 
+            // lblRece
+            // 
+            this.lblRece.AutoSize = true;
+            this.lblRece.Location = new System.Drawing.Point(36, 65);
+            this.lblRece.Name = "lblRece";
+            this.lblRece.Size = new System.Drawing.Size(72, 25);
+            this.lblRece.TabIndex = 1;
+            this.lblRece.Text = "收件人";
+            // 
+            // lblSend
+            // 
+            this.lblSend.AutoSize = true;
+            this.lblSend.Location = new System.Drawing.Point(36, 106);
+            this.lblSend.Name = "lblSend";
+            this.lblSend.Size = new System.Drawing.Size(72, 25);
+            this.lblSend.TabIndex = 2;
+            this.lblSend.Text = "寄件人";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtTitle.Location = new System.Drawing.Point(117, 22);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(229, 34);
+            this.txtTitle.TabIndex = 3;
+            this.txtTitle.Text = "請輸入信件主題";
+            // 
+            // txtRece
+            // 
+            this.txtRece.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtRece.Location = new System.Drawing.Point(117, 62);
+            this.txtRece.Name = "txtRece";
+            this.txtRece.Size = new System.Drawing.Size(229, 34);
+            this.txtRece.TabIndex = 4;
+            this.txtRece.Text = "請輸入收件人姓名與稱謂";
+            // 
+            // txtSend
+            // 
+            this.txtSend.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtSend.Location = new System.Drawing.Point(117, 103);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(229, 34);
+            this.txtSend.TabIndex = 5;
+            this.txtSend.Text = "請輸入你的姓名";
+            // 
+            // lblContent
+            // 
+            this.lblContent.AutoSize = true;
+            this.lblContent.Location = new System.Drawing.Point(36, 155);
+            this.lblContent.Name = "lblContent";
+            this.lblContent.Size = new System.Drawing.Size(92, 25);
+            this.lblContent.TabIndex = 6;
+            this.lblContent.Text = "信件內容";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(41, 193);
+            this.txtContent.MinimumSize = new System.Drawing.Size(500, 100);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ReadOnly = true;
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(594, 233);
+            this.txtContent.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(405, 103);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(107, 34);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "產生內容";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(529, 103);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(106, 34);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "儲存結果";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // lblChange
+            // 
+            this.lblChange.AutoSize = true;
+            this.lblChange.Location = new System.Drawing.Point(400, 22);
+            this.lblChange.Name = "lblChange";
+            this.lblChange.Size = new System.Drawing.Size(112, 25);
+            this.lblChange.TabIndex = 10;
+            this.lblChange.Text = "須變更內容";
+            this.lblChange.Visible = false;
+            // 
+            // txtChange
+            // 
+            this.txtChange.Location = new System.Drawing.Point(405, 56);
+            this.txtChange.Name = "txtChange";
+            this.txtChange.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtChange.Size = new System.Drawing.Size(229, 34);
+            this.txtChange.TabIndex = 11;
+            this.txtChange.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.Controls.Add(this.txtChange);
+            this.Controls.Add(this.lblChange);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.lblContent);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtRece);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblSend);
+            this.Controls.Add(this.lblRece);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "寄信助理";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblRece;
+        private System.Windows.Forms.Label lblSend;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtRece;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Label lblContent;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblChange;
+        private System.Windows.Forms.TextBox txtChange;
+    }
+}
+
---
+++ ChatGPT_hw/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace ChatGPT_hw
+{
+    public partial class Form1: Form
+    {
+        private string apiKey = "";
+        private List<object> chatHistory = new List<object>();
+        private int inputCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing; // 關閉後儲存
+            this.TxtHint(); // 文字提示
+            btnSend.Click += btnSend_Click;
+            btnSave.Click += btnSave_Click;
+        }
+        
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string userInput = "主題: "+txtTitle.Text.Trim() + Environment.NewLine+ "收件人: " +txtRece.Text.Trim() + Environment.NewLine + "寄件人: "+txtSend.Text.Trim();
+            if (string.IsNullOrEmpty(userInput)||(inputCount<3))
+            {
+                MessageBox.Show("請輸入完整資訊！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (string.IsNullOrEmpty(txtChange.Text) && (txtChange.Visible == true))
+            {
+                MessageBox.Show("請輸入更新資訊！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else if(inputCount == 3&& (txtChange.Visible == true))
+            {
+                userInput += Environment.NewLine + "請修改這幾點: " +txtChange.Text.Trim();
+            }
+            chatHistory.Add(new { role = "user", content = userInput });
+            btnSend.Enabled = false; // 禁用按鈕，防止多次點擊
+
+            string response = GetOpenAIResponse();
+            txtContent.Text=response;
+            btnSend.Enabled = true; // 啟用按鈕
+
+            lblChange.Visible = true;
+            txtChange.Visible = true;
+            btnSave.Visible = true;
+            btnSave.Enabled = true;
+        }
+
+        private int fileNum=0;
+        private void btnSave_Click(object sender, EventArgs e) {
+            string FilePath = "紀錄"+fileNum.ToString()+".txt";
+            try
+            {
+                using (StreamWriter writer1 = new StreamWriter(FilePath, false, Encoding.UTF8))
+                {
+                    writer1.WriteLine(txtContent.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("儲存紀錄失敗" + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            btnSave.Enabled = false;
+        }
+
+        //OpenAI問問題
+        private bool isSystemMessageAdded = false;
+        private string GetOpenAIResponse()
+        {
+            var client = new RestClient("https://api.openai.com/");
+            var request = new RestRequest("v1/chat/completions", Method.Post);
+            request.AddHeader("Authorization", "Bearer " + apiKey);
+            request.AddHeader("Content-Type", "application/json");
+
+            if (!isSystemMessageAdded)
+            {
+                chatHistory.Insert(0, new { role = "system", content = "請你幫忙撰寫信件，產生出信件的標題與內容。不需要列出收件人與寄件人。" });
+                isSystemMessageAdded = true;
+            }
+
+            var requestBody = new
+            {
+                model = "gpt-4",
+                messages = chatHistory,
+                max_tokens = 1000
+            };
+
+            request.AddJsonBody(JsonConvert.SerializeObject(requestBody));
+
+            var response = client.Execute(request);
+            if (response.IsSuccessful)
+            {
+                dynamic jsonResponse = JsonConvert.DeserializeObject(response.Content);
+                string aiResponse = jsonResponse.choices[0].message.content.ToString();
+                chatHistory.Add(new { role = "assistant", content = aiResponse }); // 存入歷史紀錄
+                return aiResponse;
+            }
+            else
+            {
+                return "發生錯誤：" + response.StatusCode + " - " + response.Content;
+            }
+
+        }
+
+        //文字提示
+        private void TxtHint() {
+            txtTitle.GotFocus += txtTitle_GotFocus;
+            txtTitle.LostFocus += txtTitle_LostFocus;
+            txtRece.GotFocus += txtRece_GotFocus;
+            txtRece.LostFocus += txtRece_LostFocus;
+            txtSend.GotFocus += txtSend_GotFocus;
+            txtSend.LostFocus += txtSend_LostFocus;
+        }
+        private void txtTitle_GotFocus(object sender, EventArgs e)
+        {
+            if (txtTitle.Text == "請輸入信件主題")
+            {
+                txtTitle.Text = "";
+                txtTitle.ForeColor = Color.Black;
+                inputCount++;
+            }
+        }
+        private void txtTitle_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTitle.Text))
+            {
+                txtTitle.Text = "請輸入信件主題";
+                txtTitle.ForeColor = Color.Gray;
+                inputCount--;
+            }
+        }
+        private void txtRece_GotFocus(object sender, EventArgs e)
+        {
+            if (txtRece.Text == "請輸入收件人姓名與稱謂")
+            {
+                txtRece.Text = "";
+                txtRece.ForeColor = Color.Black;
+                inputCount++;
+            }
+        }
+        private void txtRece_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRece.Text))
+            {
+                txtRece.Text = "請輸入收件人姓名與稱謂";
+                txtRece.ForeColor = Color.Gray;
+                inputCount--;
+            }
+        }
+        private void txtSend_GotFocus(object sender, EventArgs e)
+        {
+            if (txtSend.Text == "請輸入你的姓名")
+            {
+                txtSend.Text = "";
+                txtSend.ForeColor = Color.Black;
+                inputCount++;
+            }
+        }
+        private void txtSend_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtSend.Text))
+            {
+                txtSend.Text = "請輸入你的姓名";
+                txtSend.ForeColor = Color.Gray;
+                inputCount--;
+            }
+        }
+
+
+        //關閉畫面，並儲存紀錄
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveChatHistoryToFile();
+        }
+        private string chatLogFilePath = "前次歷史紀錄.txt";
+        private void SaveChatHistoryToFile() 
+        {
+            try
+            {
+                using (StreamWriter writer2 = new StreamWriter(chatLogFilePath, false, Encoding.UTF8))
+                {
+                    for (int i = 0; i < chatHistory.Count; i ++)
+                    {
+                        dynamic msg = chatHistory[i];
+
+                        if (msg.role == "system") continue;
+
+                        if (msg.role == "user")
+                        {
+                            writer2.WriteLine(msg.content);
+                        }
+                        else if (msg.role == "assistant")
+                        {
+                            writer2.WriteLine(msg.content);
+                        }
+                        writer2.WriteLine("---");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("儲存聊天紀錄失敗" + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
